52c23501fe69684aef1588d7a680b9238d680607
asalas <67822688+andrewjsalas@users.noreply.github.com>
2022-03-09 20:05:29 -0800

    display-firebase-data-plus-view-more-logic
    Created the logic to display mock data from firestore along with created a view more button that displays all  or less of the information.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,80 @@
 import { db } from './firebase.config'
 import { useState, useEffect } from 'react';
+import { collection, onSnapshot, snapshotEqual } from 'firebase/firestore';
 import './App.css';
+import Header from './Header';
 
 function App() {
+  const [workouts, setWorkouts] = useState([])
+  const [form, setForm] = useState({
+    exercise: '',
+    sets: '',
+    reps: '',
+    rest: '',
+    notes: '',
+  })
+
+  const [popupActive, setPopupActive] = useState(false)
+
+  // Setup reference towards firebase collection
+  const workoutsCollectionRef = collection(db, 'workouts')
+
+  useEffect(() => {
+    onSnapshot(workoutsCollectionRef, snapshot => {
+      setWorkouts(snapshot.docs.map(doc => {
+        return {
+          id: doc.id,
+          viewing: false, 
+          ...doc.data()
+        }
+      }))
+    })
+  }, [])
+
+  // Shows the entire card aftering pressing 'View more'
+  const handleView = id => {
+    const workoutsClone = [...workouts]
+
+    workoutsClone.forEach(workout => {
+      if (workout.id === id) {
+        workout.viewing = !workout.viewing 
+      } else {
+        workout.viewing = false
+      }
+    })
+
+    setWorkouts(workoutsClone)
+  }
+
   return (
     <div className="App">
-      workout app
+      <Header />
+
+      <button>Add Exercise</button>
+
+      <div className='workouts'>
+        { workouts.map((workouts, i) => (
+          <div className='workout' key={workouts.id}>
+            <h2>{workouts.exercise}</h2>
+
+            { workouts.viewing && <div>
+              <h3>{workouts.sets}</h3>  
+              <h3>{workouts.reps}</h3>  
+              <h3>{workouts.rest}</h3>
+              <div>
+                <p dangerouslySetInnerHTML={{ __html: workouts.notes }}></p>
+              </div>
+            </div>}
+            
+            <div className='buttons'>
+              <button onClick={() => handleView(workouts.id)}
+                >View { workouts.viewing ? 'less': 'more' } </button>  
+              <button className='remove'>Remove</button>  
+            </div>    
+          </div>
+
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <h1>Fuerte</h1>
+      <h3>Exercise Creator</h3>
+    </div>
+  )
+}
+
+export default Header
